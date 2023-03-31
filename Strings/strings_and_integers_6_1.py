d9ce64f0b42ab37d4cdc130cddf96ad24ec63297
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-03-31 10:37:18 +0800

    add: strings to integer=>6.1

---
+++ Strings/strings_and_integers_6_1.py
@@ -0,0 +1,37 @@
+def int_to_string(x): 
+    negative = False
+    if x < 0: 
+        negative = True
+        x *= -1
+
+    s = []
+    while True: 
+        s.append(chr(ord('0') + x % 10))
+        x = x // 10
+        if x == 0: 
+            break
+
+    return ('-' if negative else '') + ''.join(s[::-1])
+
+
+
+def string_to_int(y):
+    negative = False
+    start = 0
+    res = 0
+    if y[0] == '-':
+        negative = True
+        start = 1
+
+    for i in range(start, len(y)):
+        digit = ord(y[i]) - ord('0')
+        if 0 <= digit <= 9: 
+            res = res * 10 + digit
+
+        else: 
+            raise ValueError("non numeric character found")
+
+    return (res) if negative else (-1 * res)
+
+print(int_to_string(10))
+print(string_to_int("432143"))
