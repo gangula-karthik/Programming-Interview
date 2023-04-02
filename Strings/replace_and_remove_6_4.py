a5c4a31300ce7449ed7dd997d86481f2d6716d7d
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-04-03 07:29:13 +0800

    add: remove and replace=>6.4

---
+++ Strings/replace_and_remove_6_4.py
@@ -0,0 +1,30 @@
+def replace_and_remove(s, size): 
+    # forward iteration
+    write_idx, a_count = 0, 0
+    for i in range(size): 
+        if s[i] != 'b': 
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a': 
+            a_count += 1
+
+    # backward iteration
+    cur_idx = write_idx - 1
+    write_idx += a_count - 1
+    final_size = write_idx + 1
+
+    while cur_idx >= 0: 
+        if s[cur_idx] == 'a': 
+            s[write_idx] = 'd'
+            s[write_idx - 1] = 'd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[cur_idx]
+            write_idx -= 1
+
+        cur_idx -= 1
+    return final_size
+
+
+# test cases
+print(replace_and_remove(['a', 'c', 'a', 'a', '', ''], 6))
